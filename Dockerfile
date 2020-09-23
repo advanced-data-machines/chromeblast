f752ee49be802f48921deac0636641f7e9bc9f7c
sjbeskur <sam.beskur@gmail.com>
2020-09-23 17:32:52 -0600

    initial

---
+++ Dockerfile
@@ -0,0 +1,21 @@
+FROM python:latest
+
+WORKDIR /app
+
+RUN apt-get update
+RUN apt-get install vim jq -y
+# install gRPC curl program
+RUN wget https://github.com/fullstorydev/grpcurl/releases/download/v1.7.0/grpcurl_1.7.0_linux_x86_64.tar.gz
+RUN tar -xvf grpcurl_1.7.0_linux_x86_64.tar.gz
+
+# home foyer protocol stubs
+ADD google/ /app/google
+
+
+COPY get_tokens.py .
+COPY run.sh .
+
+RUN pip3.8 install gpsoauth
+
+# docker run --rm -it chromeblast <gmail_user> <password> <chromeblastsa@-290220.iam.gserviceaccount.com>
+ENTRYPOINT [ "./run.sh" ]
---
+++ run.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+username=$1  # your google / gmail email
+password=$2  # your google / gmail password
+service_username=$3  # create this under the home foyer: https://myaccount.google.com/apppasswords)
+
+# pull the access token from the result
+python3 get_tokens.py $username $password $service_username | grep "Access token:" | cut -d':' -f2 > /tmp/access_token
+
+
+ACCESS_TOKEN=$(cat /tmp/access_token)
+
+
+./grpcurl -H "authorization: Bearer ${ACCESS_TOKEN}" \
+  -import-path ./ \
+  -proto ./google/internal/home/foyer/v1.proto \
+  googlehomefoyer-pa.googleapis.com:443 \
+  google.internal.home.foyer.v1.StructuresService/GetHomeGraph
+  | jq '.home.devices[] | {deviceName, localAuthToken}'
+
+echo "Access_Token = ${ACCESS_TOKEN}"
+
+# curl http://10.80.4.35:8008/setup/eureka_info
+# curl http://10.80.4.35:8008/setup/supported_timezones
+# curl http://10.80.4.35:8008/setup/supported_locales
+
+
