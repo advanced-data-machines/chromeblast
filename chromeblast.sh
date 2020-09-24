95453364711303a2549af964fe164ce08db6525c
sjbeskur <sam.beskur@gmail.com>
2020-09-23 18:06:06 -0600

    check image exist chromeblast.sh

--- chromeblast.sh
+++ chromeblast.sh
@@ -3,6 +3,10 @@ username=$1  # your google / gmail email
 password=$2  # your google / gmail password
 service_username=$3  # create this under the home foyer: https://myaccount.google.com/apppasswords)
 
-docker build -t chromeblast .
+
+if [ -z "$(docker images -q chromeblast)" ]; then
+  echo "Building chromeblast"
+  docker build -t chromeblast .
+fi
 
 docker run --rm -it chromeblast $username $password $service_username
