829e021d1d9504d3f08632122aa8832037b18de6
sjbeskur <sam.beskur@gmail.com>
2020-09-24 11:56:28 -0600

    more comments

--- scripts/run.sh
+++ scripts/run.sh
@@ -12,7 +12,7 @@ ACCESS_TOKEN=$(cat /tmp/access_token)
 echo
 echo "Access_Token = ${ACCESS_TOKEN}"
 echo
-echo "localAuthToken should be here: "
+echo "localAuthToken = "
 ./grpcurl -H "authorization: Bearer ${ACCESS_TOKEN}" \
   -import-path ./ \
   -proto ./google/internal/home/foyer/v1.proto \
@@ -21,12 +21,20 @@ echo "localAuthToken should be here: "
   | grep localAuthToken
 
 #  | jq '.home.devices[] | {deviceName, localAuthToken}'
-
 echo  
-echo "curl http://10.80.4.35:8008/setup/eureka_info"
-echo "curl http://10.80.4.35:8008/setup/supported_timezones"
-echo "curl http://10.80.4.35:8008/setup/supported_locales"
+echo "If above is empty then likely your device is not yet registered with Google home foyer, etc."
+
+echo "******************************************************"
+echo "These api calls with work with out a localAuthToken"
+echo "******************************************************"
+echo "curl http://<ip_of_chromecast>:8008/setup/eureka_info"
+echo "curl http://<ip_of_chromecast>:8008/setup/supported_timezones"
+echo "curl http://<ip_of_chromecast>:8008/setup/supported_locales"
 echo 
-echo "curl -H 'cast-local-authorization-token: REPLACE_WITH_LOCAL_AUTH_TOKEN' --verbose --insecure https://10.80.4.35:8008/setup/bluetooth/status"
+echo "******************************************************"
+echo "Example GET request with localAuthToken (fails)"
+echo "******************************************************"
+echo "curl -H 'cast-local-authorization-token: REPLACE_WITH_LOCAL_AUTH_TOKEN' --verbose --insecure https://<ip_of_chromecast>:8008/setup/bluetooth/status"
+echo 
 echo
 echo
